8e1619dcf2b0a95a72ce4d47101c8af33965cfe7
Keith Kaplan <keithkaplan0@gmail.com>
2024-08-03 17:16:27 -0700

    test(ArrayUtil): Test building arrays of zeros

--- test/ArrayUtil.purs
+++ test/ArrayUtil.purs
@@ -3,6 +3,8 @@ module Test.ArrayUtil (run) where
 import Prelude
 import ArrayUtil as A
 
+import Data.Array (length)
+import Data.Foldable (all)
 import Data.Int (pow)
 import Effect (Effect)
 import Effect.Class.Console (log)
@@ -11,10 +13,35 @@ import Test.QuickCheck (Result, quickCheck, (<?>))
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
 
 newtype PosInt = PosInt Int
+newtype PosInt50 = PosInt50 Int
+newtype NatNum2k = NatNum2k Int
 
 instance Arbitrary PosInt where
-    arbitrary = PosInt <<< (_ `mod` n) <$> arbitrary
-        where n = pow 2 31
+    arbitrary = PosInt <<< (_ + 1) <<< (_ `mod` maxInt) <$> arbitrary
+        where maxInt = pow 2 31 - 1
+
+instance Arbitrary PosInt50 where
+    arbitrary = PosInt50 <<< (_ + 1) <<< (_ `mod` 50) <$> arbitrary
+
+instance Arbitrary NatNum2k where
+    arbitrary = NatNum2k <<< (_ `mod` 2000) <$> arbitrary
+
+prop_ZeroArray :: NatNum2k -> Result
+prop_ZeroArray (NatNum2k len) =
+    (len == length arr) && (all (_ == 0) arr) <?> msg
+    where
+    arr = A.buildZeroArray len
+    msg = "len = " <> show len
+       <> "\nbuildZeroArray len:\n" <> show arr
+
+prop_ZeroArray2D :: PosInt50 -> PosInt50 -> Result
+prop_ZeroArray2D (PosInt50 rows) (PosInt50 cols) =
+    (rows == length arr) && (all (\row -> length row == cols) arr) <?> msg
+    where
+    arr = A.buildZeroArray2D rows cols
+    msg = "rows = " <> show rows <> "\n"
+       <> "cols = " <> show cols <> "\n"
+       <> "buildZeroArray2D rows cols:\n" <> show arr
 
 prop_randIntRange :: PosInt -> Result
 prop_randIntRange (PosInt n) = x >= 0 && x < n <?> msg
@@ -31,6 +58,10 @@ prop_randInt1 = x == 0 <?> msg
 
 run :: Effect Unit
 run = do
+    log "Test: buildZeroArray"
+    quickCheck prop_ZeroArray
+    log "Test: buildZeroArray2D"
+    quickCheck prop_ZeroArray2D
     log "Test: 0 <= randInt n < n (for positive n)"
     quickCheck prop_randIntRange
     log "Test: randInt 1 == 0"
