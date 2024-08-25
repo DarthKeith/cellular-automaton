6cc4e446cf51d026b4116cbc89f38cf27461a8f8
Keith Kaplan <keithkaplan0@gmail.com>
2024-08-24 19:33:57 -0700

    refactor: Rebuild control panel HTML

--- src/dev/ControlPanel.purs
+++ src/dev/ControlPanel.purs
@@ -3,6 +3,7 @@ module ControlPanel (component) where
 import Prelude
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 
 type State = Unit
 type Action = Unit
@@ -15,11 +16,44 @@ component =
         , eval: H.mkEval H.defaultEval
         }
 
+text :: forall w i. String -> Array (HH.HTML w i)
+text s = [HH.text s]
+
+label :: forall w i. String -> HH.HTML w i
+label s = HH.label_ $ text s
+
 render :: forall cs m. State -> H.ComponentHTML Action cs m
-render _ = HH.div_
-    [ HH.label_
-        [ HH.text "Contorl Panel" ]
-    , HH.button_
-        [ HH.text "Button" ]
+render _ = HH.div [HP.id "ctrl-panel"]
+    [ HH.div [HP.class_ $ HH.ClassName "ctrl-group"]
+        [ label "Model Controls"
+        , HH.div [HP.class_ $ HH.ClassName "btn-group"]
+            [ label "Number of States"
+            , HH.div_
+                [ HH.button_ $ text "2"
+                , HH.button_ $ text "3"
+                , HH.button_ $ text "4"
+                , HH.button_ $ text "5"
+                ]
+            ]
+        , HH.button [HP.id "rule-btn"] $ text "Change Rule"
+        , HH.button [HP.id "reset-btn"] $ text "Randomize Cells"
+        ]
+    , HH.div [ HP.class_ $ HH.ClassName "ctrl-group" ]
+        [ label "Display Controls"
+        , HH.div [HP.class_ $ HH.ClassName "btn-group"]
+            [ label "Cell Size"
+            , HH.div_
+                [ HH.button_ $ text "1"
+                , HH.button_ $ text "2"
+                , HH.button_ $ text "3"
+                , HH.button_ $ text "4"
+                , HH.button_ $ text "5"
+                , HH.button_ $ text "10"
+                , HH.button_ $ text "20"
+                ]
+            ]
+        , HH.button [HP.id "color-btn"] $ text "Change Colors"
+        , HH.button [HP.id "pause-btn"] $ text "Play/Pause"
+        ]
     ]
 
